be9dcec232626b48aa375970bf5ebc6277e9a558
mwenda <mwendakennedy3@gmail.com>
2021-02-12 13:51:18 +0300

    Changed content of creating schema

--- DDL scripts/creating_schema.sql
+++ DDL scripts/creating_schema.sql
@@ -1,15 +1,1 @@
-create table nakala_schema.Kpi_Performance_Tracker(
-	sbu_code  						CHAR(10)      	 NOT NULL,
-	role_code 						VARCHAR(20)  	 NOT NULL,
-	staff_position 					CHAR(50) 		 NOT NULL,
-	kpi_code 						VARCHAR(30)    	 NOT NULL,
-	staff_kpi_objective 			VARCHAR(50) 	 NOT NULL,
-	perf_target 					FLOAT 			 NOT NULL,
-	perf_actual  					FLOAT 	         NOT NULL,
-	perf_score_prediction 			FLOAT ,
-	perf_score_percentage 			INT              NOT NULL,
-	kpi_season_change_percentage 	INT              NOT NULL,
-	perf_status 					CHAR(15) ,
-	sbu_dependency 					CHAR(15),
-	PRIMARY KEY (kpi_code)
-);
+create schema nakala_schema;
